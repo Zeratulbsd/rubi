506f8b9d8478a3133293ed3c2ebfae6eef484358
Zeratulbsd <rvilchiscruz@gmail.com>
2020-01-14 22:29:33 -0600

    first commit

---
+++ menu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os
+print("Menu segunda oportunidad  Unidad 1")
+print ("materia:Seguridad informatica")
+print("Docente: Juan Monrroy")
+print("Alumna:Rubi guadalupe serapio herrejon")
+aver =0
+while aver != 6 and aver <7:
+   print("1 vigenere")
+   print("2 transposicion")
+   print("3 rot13")
+   print("4 cesar")
+   print("6 salir")
+   aver=int(input("Ingresa tu opcion "))
+   if aver == 1:  
+       os.system("python vigenere.py")
+   if aver == 2:  
+       os.system("transposicion.py")
+   if aver == 3:  
+       os.system("pythn rot13.py")
+   if aver == 4:  
+       os.system("python Menu.py")
+   if aver == 5:  
+       os.system("python vigenere.py")
+   
---
+++ rot13.py
@@ -0,0 +1,28 @@
+def rot13(palabra):
+    Cifrado = ''
+    for i in palabra:
+        buff = ord(i)
+        if (buff >= 65 and buff <= 90) or (buff >= 97 and buff <= 122):
+            if ((buff + 13 > 90 and buff + 13 <= 103) or (buff + 13 > 122 and buff + 13 <= 135)):
+                Cifrado += chr(buff -13)
+            else:
+                Cifrado += chr(buff + 13)
+    print (Cifrado)
+    
+def atbash(palabra):
+    V1 = "abcdefghijklm"
+    V2 = "zyxwvutsrqpon"
+    
+    Buff = ""
+    
+    for i in range(len(palabra)):
+        for a in range(len(V1)):
+            if V1[a] == palabra[i]:
+                Buff += V2[a]
+            elif V2[a] == palabra[i]:
+                Buff += V1[a]
+    print (Buff)
+ 
+#atbash("criptografia")
+rot13("Criptografia")
+ 
